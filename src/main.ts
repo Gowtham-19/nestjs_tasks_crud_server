e72b6a0bd5ed45a9132f8ec923ef8088141a91ca
unknown <gowtham>
2022-09-18 13:06:36 +0530

    enabled cores

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { TransformInterceptor } from './transform.intercepter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
